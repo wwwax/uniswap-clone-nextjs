6fc4cb5dff173cfbe010ec0a733c92ce9f6acdb6
Max Ivanov <waxivanov@gmail.com>
2023-06-30 16:45:47 +0300

    feat: add bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { JSX } from 'react';
 import { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import Head from 'next/head';
 
 import '../styles/global.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
